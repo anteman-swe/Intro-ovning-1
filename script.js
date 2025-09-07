48fed70552f375a9bff4fa49a9bdfd674ddbd104
Anders Eriksson <anteman.swe@gmail.com>
2025-09-07 19:41:14 +0200

    Lade till nödvändiga filer

---
+++ script.js
@@ -0,0 +1,14 @@
+const myHeadline = document.getElementById("headline");
+
+document.getElementById("button").addEventListener("click", changePage);
+
+function changePage(){
+    if (myHeadline.innerHTML == "Hello World!") {
+        myHeadline.style.color = "red";
+        myHeadline.innerHTML = "Goodbye World!";
+    } else {
+        myHeadline.style.color = "blue";
+        myHeadline.innerHTML = "Hello World!";
+    }
+    
+}
