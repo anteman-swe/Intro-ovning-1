7985c7374913ea51dd56b9d38f8551d2eb1a3a0f
Anders Eriksson <anteman.swe@gmail.com>
2025-09-10 16:29:03 +0200

    Lagt till kommentarer och små ändringar

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const myHeadline = document.getElementById("headline");
-const button = document.getElementById("button");
-const hello = "Hello World!";
-const goodbye = "Goodbye World!";
-const helloColor = "blue";
-const goodbyeColor = "red";
-const buttonTextHello ="Klicka mig!"
-const buttonTextGoodbye ="Klicka mig igen!"
+const myHeadline = document.getElementById("headline"); // Väljer min rubrik, hade kunnat använda querySelector då det bara finns en "h1" men querySelector hittar bara den första förekomsten;
+const button = document.getElementById("button"); // Väljer min knapp "button", hade kunnat använda querySelector då det bara finns en "button" men queryselector hittar bara den första förekomsten;
+const hello = "Hello World!"; //  konstant för vad man ska säga utan klick eller efter var annat klick. 
+const goodbye = "Goodbye World!"; // konstant för vad man säger efter klick.
+const helloColor = "blue";  // Vilken färg på Hello
+const goodbyeColor = "red"; // Vilken färg på Goodbye
+const buttonTextHello ="Klicka mig!"; // Knapptext när "Hello World!" visas
+const buttonTextGoodbye ="Klicka mig igen!"; // Kanpptext när "Goodbye World!" visas
 
-myHeadline.innerHTML = hello;
-button.innerHTML = buttonTextHello
+myHeadline.innerHTML = hello; // Sätt text första gången till min rubrik
+button.innerHTML = buttonTextHello // Sätt text till knappen för första gången
 
-button.addEventListener("click", changePage);
+button.addEventListener("click", changePage); // Sätt en lyssnare som lyssnar efter musklick på knappen och då kör funktionen "changePage"
 
-function changePage(){
+function changePage(){  // Funktion som växlar text och färg i rubriken vid klick på knappen
     if (myHeadline.innerHTML == hello) {
         myHeadline.style.color = goodbyeColor;
         myHeadline.innerHTML = goodbye;
